08627a004b16a1acf9f444ef4e542a784d30ce7c
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-03-17 14:56:50 +0200

    Base page and some other things complited

--- src/test/java/PageObjects/BasePage.java
+++ src/test/java/PageObjects/BasePage.java
@@ -1,4 +1,28 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    public WebDriver driver;
+    public BasePage(WebDriver driver){
+        this.driver=driver;
+    }
+    public WebElement findElement(String locator){
+        return driver.findElement(By.xpath(locator));}
+
+//    int basicTime = 10;
+//
+//    public WebElement waitElementToBeVisible (String locator){
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(basicTime));
+//        return wait.until(ExpectedCondition);
+//    }
+
+
+
 }
