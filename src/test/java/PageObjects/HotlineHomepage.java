a5683df5a0c9ce5721135757bf2f2361b51ceb64
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-03-24 16:08:20 +0200

    Catalog test

--- src/test/java/PageObjects/HotlineHomepage.java
+++ src/test/java/PageObjects/HotlineHomepage.java
@@ -1,8 +1,13 @@
 package PageObjects;
 
+import Tests.TestInit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.List;
 
 
 public class HotlineHomepage extends BasePage {
@@ -20,11 +25,16 @@ public class HotlineHomepage extends BasePage {
 
     public WebElement searchField(){return driver.findElement(By.xpath("//*[@type = 'text' and @autocomplete='off']"));}
     public void clickOnSearchButton(){waitElementToBeVisible("//*[@class = 'search__btn flex middle-xs center-xs']").click();}
-
-
-
-
-
-
+    public void clickOnCatalogButton(){waitElementToBeVisible("//*[@class='header-catalog-button']").click();}
+    public void clickOnSomeButtons(){waitElementsToBeVisible("//*[@class = 'menu-main__item']").forEach(WebElement::isSelected);}
+    public void showAllElements() {
+        List<WebElement> elements = waitElementsToBeVisible("//*[@class = 'menu-main__item']");
+        for (WebElement element : elements) {
+            Actions action = new Actions(driver);
+            action.moveToElement(element).perform();
+            TestInit testInit = new TestInit();
+            testInit.sleep(5);
+        }
+    }
 
 }
