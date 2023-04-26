32e25b8b2bcdb7eea83c224b07ad68701b0a32bb
Oleksii Venedchuk <oleksijvenedchuk@gmail.com>
2023-04-26 12:33:22 +0300

    some fixes

--- src/test/java/Tests/PopularForYouBlockTest.java
+++ src/test/java/Tests/PopularForYouBlockTest.java
@@ -10,7 +10,9 @@ public class PopularForYouBlockTest extends TestInit{
         HotlineHomepage hotlineHomepage = new HotlineHomepage(driver);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         hotlineHomepage.goToHotline();
+        sleep(5);
         js.executeScript("arguments[0].scrollIntoView();", hotlineHomepage.popularForYouBlockOfButtons());
+        sleep(5);
         hotlineHomepage.ClickForYouBlockOfButtons();
 
 
